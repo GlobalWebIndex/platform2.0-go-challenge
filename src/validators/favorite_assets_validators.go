8110c7733ceb17df0e104d298320ecdf4ceb7c8a
Archontellis Rafail Sotirchellis <arsotirchellis@gmail.com>
2019-12-16 20:14:15 +0200

    redundant file removal

--- src/validators/favorite_assets_validators.go
+++
@@ -1,8 +0,0 @@
-package validators
-
-// type InsightValidator struct {
-// 	FavoriteActionToValidate struct {
-// 		Title       string `json:"title" binding:"exists"`
-// 		Description string `json:"description" binding:"exists"`
-// 	} `json:"insight"`
-// }
